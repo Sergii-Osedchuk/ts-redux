b431ec7e4ebf42836e632625d4a9bde798507059
Sergio <norgestream@gmail.com>
2024-11-26 15:24:59 +0200

    add ability to add and remove items from cart

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,9 +1,26 @@
+import { useCartDispatch, useCartSelector } from "../store/hooks";
+import {type CartItem, addToCart, removeFromCart } from "../store/cart-slice";
+
 export default function CartItems() {
+  const cartItems = useCartSelector(state => state.cart.items);
+  const dispatch = useCartDispatch();
+
+  const totalPrice = cartItems.reduce((val, item) => val + item.price * item.quantity, 0)
+  const formattedTotalPrice = totalPrice.toFixed(2)
+
+  function handleAddToCart(item: CartItem) {
+    dispatch(addToCart(item))
+  }
+
+  function handleRemoveFromCart(id: string) {
+    dispatch(removeFromCart(id));
+  }
+
   return (
     <div id="cart">
-      <p>No items in cart!</p>
+      {!cartItems.length && <p> No items in cart!</p>}
 
-      {/* <ul id="cart-items">
+      <ul id="cart-items">
           {cartItems.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
@@ -23,11 +40,11 @@ export default function CartItems() {
               </li>
             );
           })}
-        </ul> */}
+        </ul>
 
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+      <p id="cart-total-price">
+        Cart Total: <strong>${formattedTotalPrice}</strong>
+      </p>
     </div>
   );
 }
